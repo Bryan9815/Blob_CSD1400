06bfce8ce9f6baaba0bb2c50e23288256be857dd
AceTreinerLeo <leonardkoh@yahoo.com>
2020-10-21 10:24:58 +0800

    Created variables and functions for positioning and drawing bosses
    Signed-off-by: AceTreinerLeo <leonardkoh@yahoo.com>

---
+++ CProcessing/src/Bosses/Boss.c
@@ -0,0 +1,15 @@
+#include <cprocessing.h>
+#include "Boss.h"
+
+void BossPositioning(Boss currentboss) //function to set position of boss(es)
+{
+	//temporary
+	currentboss.Position = CP_Vector_Set(CP_System_GetWindowWidth() / 2, CP_System_GetWindowHeight() / 2);
+}
+
+void BossDraw(Boss currentboss) //function to draw boss(es)
+{
+	CP_Color BossColor = CP_Color_Create(0, 0, 255, 255);
+	CP_Settings_Fill(BossColor);
+	CP_Graphics_DrawCircle(currentboss.Position.x, currentboss.Position.y, currentboss.Size);
+}
---
+++ CProcessing/src/Bosses/Boss.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <cprocessing.h>
+
+typedef struct
+{
+	CP_Vector Position; //Position vector of boss
+	int Health;			//Health of boss
+	int Size;			//Radius of boss for drawing
+} Boss;
+
+Boss ArmorSlime; //currently only 1 boss
+
+void BossPositioning(Boss currentboss);
+void BossDraw(Boss currentboss);
