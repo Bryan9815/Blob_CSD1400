5fbe099c2d281e060f7aa797151cf93a12cf2db0
legendarydeadweight <necropsychopath04@gmail.com>
2020-10-23 14:41:32 +0800

    Added player state, arrow trigger

--- CProcessing/src/Player/player.c
+++ CProcessing/src/Player/player.c
@@ -5,146 +5,212 @@
 
 CP_Color backgroundColour;
 //Player newPlayer;
-
-void SetPlayer(Player* player) //Default settings
+void CreatePlayer(Player* newPlayer) //Default Variables
 {
+	//Set player state
+	playerState = STILL;
+
+	playerColor = CP_Color_Create(255, 255, 255, 255);
+	//sprite = CP_Image_Load(".dioface.png");
+
 	//Player Stats and Position
-	//player.sprite = CP_Image_Load("./dioface.png");
-	player->speed = 7.0f;
-	player->width = CP_System_GetWindowWidth() / 15.0f;
-	player->rotation = 0.0f;
-	player->position.x = CP_System_GetWindowWidth() / 2.0f;
-	player->position.y = CP_System_GetWindowWidth() / 2.0f;
-	player->velX = 0;
-	player->velY = -1;
+	newPlayer->health = 1;
+	newPlayer->width = CP_System_GetWindowWidth() / 15.0f;
+	newPlayer->rotation = 0.0f;
+	newPlayer->position = CP_Vector_Set(CP_System_GetWindowWidth() / 2.0f,
+						  (CP_System_GetWindowHeight() / 2.0f) + (CP_System_GetWindowHeight() / 4.0f));
+	newPlayer->vel = CP_Vector_Set(0, -1);
 
 	//Player dodge
-	player->numDodge = 2;
-	player->dodgeBlur = 3;
-	player->dodgeCooldown = 4;
-	player->dodgeFactor = 10.0f;
-	player->dodgeTimer = CP_System_GetWindowWidth() / 100.0f;
-	player->isDodging = 0;
+	dodgeDistance = CP_System_GetWindowWidth() / 100.0f;
+	dodgeTimer = 0.0f;
+	dodgeBlur = 3;
+	newPlayer->numDodge = 2;
+
+	//Player Arrow
+	newPlayer->playerHasArrow = 1;
+	SetArrow(&(newPlayer->arrow));
+
 }
 
-/*Physics*/
-void PlayerUpdate(Player player)
+void PlayerDraw(Player* player)
 {
-	
+	//Draw player
+	CP_Settings_Fill(playerColor);
+	CP_Graphics_DrawRectAdvanced(player->position.x, player->position.y, player->width, player->width, player->rotation, 1);
+
+	//Draw arrow
+	CP_Settings_Fill(arrowColor);
+	CP_Graphics_DrawRectAdvanced(player->arrow.position.x, player->arrow.position.y, player->arrow.width, player->arrow.width, player->rotation + 45.0f, 1);
+
+	//Draw Dio
+	//CP_Image_DrawAdvanced(sprite, player->position.x, player->position.y, dioWidth, dioHeight, 255, player->rotation);
 }
 
 void PlayerMovement(Player* player)
 {
 	CP_Settings_Background(backgroundColour);
 
-	if (CP_Input_KeyTriggered(KEY_SPACE) && (player->numDodge > 0))
+	if (playerState != DODGING)
 	{
-		player->isDodging = 1;
-		player->numDodge -= 1;
-
-	}
-	else if (CP_Input_KeyDown(KEY_W) && CP_Input_KeyDown(KEY_A))
-	{
-		player->velY = -1;
-		player->position.y += (player->velY * player->speed);
-		player->velX = -1;
-		player->position.x += (player->velX * player->speed);
-		player->rotation = -45.0f;
-	}
-	else if ((CP_Input_KeyDown(KEY_W) && CP_Input_KeyDown(KEY_D)))
-	{
-		player->velY = -1;
-		player->position.y += (player->velY * player->speed);
-		player->velX = 1;
-		player->position.x += (player->velX * player->speed);
-		player->rotation = 45.0f;
-	}
-	else if (CP_Input_KeyDown(KEY_W))
-	{
-		player->velY = -1;
-		player->position.y += (player->velY * player->speed);
-		player->velX = 0;
-		player->position.x += (player->velX * player->speed);
-		player->rotation = 0.0f;
-	}
-	else if (CP_Input_KeyDown(KEY_S) && CP_Input_KeyDown(KEY_D))
-	{
-		player->velY = 1;
-		player->position.y += (player->velY * player->speed);
-		player->velX = 1;
-		player->position.x += (player->velX * player->speed);
-		player->rotation = 135.0f;
-	}
-	else if (CP_Input_KeyDown(KEY_S) && CP_Input_KeyDown(KEY_A))
-	{
-		player->velY = 1;
-		player->position.y += (player->velY * player->speed);
-		player->velX = -1;
-		player->position.x += (player->velX * player->speed);
-		player->rotation = 225.0f;
-	}
-	else if (CP_Input_KeyDown(KEY_S))
-	{
-		player->velY = 1;
-		player->position.y += (player->velY * player->speed);
-		player->velX = 0;
-		player->position.x += (player->velX * player->speed);
-		player->rotation = 180.0f;
-	}
-
-	else if (CP_Input_KeyDown(KEY_A))
-	{
-		player->velY = 0;
-		player->position.y += (player->velY * player->speed);
-		player->velX = -1;
-		player->position.x += (player->velX * player->speed);
-		player->rotation = -90.0f;
-	}
-
-	else if (CP_Input_KeyDown(KEY_D))
-	{
-		player->velY = 0;
-		player->position.y += (player->velY * player->speed);
-		player->velX = 1;
-		player->position.x += (player->velX * player->speed);
-		player->rotation = 90.0f;
-	}
-	CP_Graphics_DrawRectAdvanced(player->position.x, player->position.y, player->width, player->width, player->rotation, 1);
-	//CP_Image_DrawAdvanced(player.sprite, player.position.x, player.position.y, player->width, player->width, 255, rotateimage);
-}
-
-//Cooldown for Dodge
-void PlayerDodgeTimer(Player* player)
-{
-	if (player->numDodge < 2)
-	{
-		player->dodgeTimer += CP_System_GetDt();
-		if (player->dodgeTimer > player->dodgeCooldown)
+		playerState = STILL;
+		if (CP_Input_KeyTriggered(KEY_SPACE) && (player->numDodge > 0))
 		{
-			player->numDodge += 1;
-			player->dodgeTimer = 0.0f;
+			playerState = DODGING;
+
+			player->numDodge -= 1;
+
+		}
+		else if (CP_Input_KeyDown(KEY_W) && CP_Input_KeyDown(KEY_A))
+		{
+			playerState = MOVING;
+			player->vel = CP_Vector_Set(-1, -1);
+			player->position.x += (player->vel.x * PLAYER_SPEED);
+			player->position.y += (player->vel.y * PLAYER_SPEED);
+			player->rotation = -45.0f;
+		}
+		else if ((CP_Input_KeyDown(KEY_W) && CP_Input_KeyDown(KEY_D)))
+		{
+			playerState = MOVING;
+			player->vel = CP_Vector_Set(1, -1);
+			player->position.x += (player->vel.x * PLAYER_SPEED);
+			player->position.y += (player->vel.y * PLAYER_SPEED);
+			player->rotation = 45.0f;
+		}
+		else if (CP_Input_KeyDown(KEY_W))
+		{
+			playerState = MOVING;
+			player->vel = CP_Vector_Set(0, -1);
+			player->position.x += (player->vel.x * PLAYER_SPEED);
+			player->position.y += (player->vel.y * PLAYER_SPEED);
+			player->rotation = 0.0f;
+		}
+		else if (CP_Input_KeyDown(KEY_S) && CP_Input_KeyDown(KEY_D))
+		{
+			playerState = MOVING;
+			player->vel = CP_Vector_Set(1, 1);
+			player->position.x += (player->vel.x * PLAYER_SPEED);
+			player->position.y += (player->vel.y * PLAYER_SPEED);
+			player->rotation = 135.0f;
+		}
+		else if (CP_Input_KeyDown(KEY_S) && CP_Input_KeyDown(KEY_A))
+		{
+			playerState = MOVING;
+			player->vel = CP_Vector_Set(-1, 1);
+			player->position.x += (player->vel.x * PLAYER_SPEED);
+			player->position.y += (player->vel.y * PLAYER_SPEED);
+			player->rotation = 225.0f;
+		}
+		else if (CP_Input_KeyDown(KEY_S))
+		{
+			playerState = MOVING;
+			player->vel = CP_Vector_Set(0, 1);
+			player->position.x += (player->vel.x * PLAYER_SPEED);
+			player->position.y += (player->vel.y * PLAYER_SPEED);
+			player->rotation = 180.0f;
+		}
+
+		else if (CP_Input_KeyDown(KEY_A))
+		{
+			playerState = MOVING;
+			player->vel = CP_Vector_Set(-1, 0);
+			player->position.x += (player->vel.x * PLAYER_SPEED);
+			player->position.y += (player->vel.y * PLAYER_SPEED);
+			player->rotation = -90.0f;
+		}
+
+		else if (CP_Input_KeyDown(KEY_D))
+		{
+			playerState = MOVING;
+			player->vel = CP_Vector_Set(1, 0);
+			player->position.x += (player->vel.x * PLAYER_SPEED);
+			player->position.y += (player->vel.y * PLAYER_SPEED);
+			player->rotation = 90.0f;
 		}
 	}
+
+	player->arrow.position = player->position;
+	PlayerDraw(player);
 }
 
-//Player Dodging
-void PlayerDodge(Player* player)
+void Dodge(Player* player)
 {
-	if (player->isDodging == 1)
+	DodgeRecharge(player);
+	if (playerState == DODGING)
 	{
 
-		if (player->dodgeBlur != 0)
+		if (dodgeBlur != 0)
 		{
-			player->dodgeBlur -= 1;
+			dodgeBlur -= 1;
 		}
 		else
 		{
-			player->dodgeBlur = 3;
-			player->isDodging = 0;
+			dodgeBlur = 3;
+			playerState = STILL;
 		}
-		player->position.x += (player->velX * (player->speed * (player->dodgeFactor / 4)));
-		player->position.y += (player->velY * (player->speed * (player->dodgeFactor / 4)));
-		CP_Graphics_DrawRectAdvanced(player->position.x, player->position.y, player->width, player->width, player->rotation, 1);
-		//CP_Image_DrawAdvanced(player->sprite, player->position.x, player->position.y, dioWidth, dioHeight, 255, rotateimage);
+		player->position.x += player->vel.x * (PLAYER_SPEED * (dodgeDistance / 4));
+		player->position.y += player->vel.y * (PLAYER_SPEED * (dodgeDistance / 4));
+		player->arrow.position = player->position;
+		PlayerDraw(player);
 	}
+}
+
+void DodgeRecharge(Player* player) //Recharge Dodge
+{
+	if (player->numDodge < 2)
+	{
+		dodgeTimer += CP_System_GetDt();
+		if (dodgeTimer > DODGE_COOLDOWN)
+		{
+			player->numDodge += 1;
+			dodgeTimer = 0.0f;
+		}
+	}
+}
+
+void ArrowTrigger(Player* player)
+{
+	if (player->playerHasArrow == 1)
+	{
+		if (CP_Input_MouseDown(MOUSE_BUTTON_LEFT))
+		{
+			if (playerState == STILL)
+			{
+				CP_Settings_Fill(CP_Color_Create(0, 0, 255, 100));
+				CP_Graphics_DrawRectAdvanced(player->arrow.position.x, player->arrow.position.y, player->arrow.width, player->arrow.width, player->rotation + 45.0f, 1);
+				if (player->arrow.forceTimer <= 30)
+				{
+					player->arrow.forceTimer += CP_System_GetDt() * 10;
+				}
+			}
+			else if (playerState == MOVING || playerState == DODGING)
+			{
+				player->arrow.forceTimer = 0.0f;
+			}
+		}
+		else if (CP_Input_MouseReleased(MOUSE_BUTTON_LEFT))
+		{
+			playerState = SHOOTING;
+			if (playerState == SHOOTING)
+			{
+				PlayerDraw(player);
+				player->arrow.dir.x = CP_Input_GetMouseX() - player->position.x;
+				player->arrow.dir.y = CP_Input_GetMouseY() - player->position.y;
+				player->arrow.force = DEFAULT_FORCE + player->arrow.forceTimer;
+				player->arrow.forceTimer = 0.0f;
+
+				//player->playerHasArrow = 0;
+
+			}
+		}
+	}
+}
+
+/*Update Player*/
+void PlayerUpdate(Player* player)
+{
+	PlayerMovement(player);
+	Dodge(player);
+	ArrowTrigger(player);
+	ArrowRelease(&(player)->arrow);
 }
--- CProcessing/src/Player/player.h
+++ CProcessing/src/Player/player.h
@@ -1,26 +1,51 @@
 #pragma once
 #include <cprocessing.h>
+#include "arrow.h"
+
+#define PLAYER_SPEED	7.0f
+#define DODGE_COOLDOWN	4	//Seconds
+#define MAX_DODGE		2
+
 typedef struct
 {
-	float		speed,
-				width,
+	int			health;
+	float		width,
 				rotation;
-	CP_Vector	position;		//Position of player
-	int			velX,			//direction/velocity along x-axis
-				velY;			//direction/velocity along y axis
-				
-	int			numDodge,		//Number of Dodges
-				isDodging,
-				dodgeBlur,		//Afterimage
-				dodgeCooldown;	//Cooldown of dodge
-	float		dodgeFactor,	//Distance travelled via dodge
-				dodgeTimer;		//Timer for dodging cooldown
-	//CP_Image	  sprite;
+	CP_Vector	position;	//Position of player
+	CP_Vector   vel;		//direction/velocity
+
+	int			numDodge;	// number of dodges
+
+	Arrow		arrow;
+	int			playerHasArrow;
 
 } Player;
 
-Player newPlayer;
+typedef enum
+{
+	MOVING,
+	DODGING,
+	STILL,
+	SHOOTING
 
-void SetPlayer(Player* player);
-void PlayerUpdate(Player player);
+} PlayerState;
+
+//CP_Image	sprite;
+CP_Color	playerColor;
+CP_Color	backgroundColour;
+
+PlayerState playerState;
+
+float dodgeTimer;		//Timer for dodging cooldown
+float dodgeDistance;	//distance travelled via dodge
+int dodgeBlur;			//Dodge blur effect
+
+void CreatePlayer(Player* player);
+void PlayerDraw(Player* player);
+
+void PlayerUpdate(Player* player);
 void PlayerMovement(Player* player);
+void Dodge(Player* player);
+void DodgeRecharge(Player* player);
+
+void ArrowTrigger(Player* player);
