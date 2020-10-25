3fa5466c9cf40a4d2749258c236222033ffc7d83
PwnJustRight <jiarong_phang@yahoo.com>
2020-10-26 01:35:25 +0900

    Deleted unnecessary calls in main Added Save/Load files //No functionality
    Signed-off-by: PwnJustRight <jiarong_phang@yahoo.com>

---
+++ CProcessing/src/GameLogic/SaveLoad.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFSIZE 255
+
+char buffer[BUFSIZE];
+
+void ReadFile()
+{
+    //FILE* fp;
+
+}
+
+void WriteFile()
+{
+   // FILE  *fp;
+    //fclose(fp);
+}
